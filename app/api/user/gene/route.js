31c7d75fe38698c04a1f7d7e0f6fbe0a9d1348f4
Kobchok Chuawong <kobchok.chuawong@gmail.com>
2025-09-17 15:38:16 +0700

    example Insert Rule based

--- app/api/user/gene/route.js
+++ app/api/user/gene/route.js
@@ -1,6 +1,7 @@
 import { ResponseModel } from "../../../../lib/model/Response";
 import { NextResponse } from "next/server";
-import { QueryRule } from "../service/rule_service";
+import { QueryRule, InsertRule } from "../service/rule_service";
+import { RuleModel } from "../../../../lib/model/Rule";
 
 export async function GET(req) {
     const { searchParams } = new URL(req.url);
@@ -33,3 +34,32 @@ export async function GET(req) {
     ResponseModel.data = data;
     return NextResponse.json(ResponseModel, { status: 200 })
 }   
+
+export async function POST(req) {
+    const body = await req.json();
+    if (!body || !body.location || !body.result_location || !body.predicted_genotype || !body.predicted_phenotype || !body.recommend) {
+        ResponseModel.status = '400'
+        ResponseModel.message = 'Invalid Data'
+        ResponseModel.data = null;
+        console.error("Invalid Data") //for Debug
+        return NextResponse.json(ResponseModel, { status: 400 }) //for User
+    }
+    RuleModel.location = body.location;
+    RuleModel.result_location = body.result_location;
+    RuleModel.predicted_genotype = body.predicted_genotype;
+    RuleModel.predicted_phenotype = body.predicted_phenotype;
+    RuleModel.recommend = body.recommend;
+
+    const response = await InsertRule(RuleModel);
+    if (response.error) {
+        ResponseModel.status = '500'
+        ResponseModel.message = 'Insert Failed' + response.error
+        ResponseModel.data = null;
+        return NextResponse.json(ResponseModel, { status: 500 })
+    }
+    console.log("Insert successful:", response.data); // Debug log
+    ResponseModel.status = '201';
+    ResponseModel.message = 'Insert Successful';
+    ResponseModel.data = response.data;
+    return NextResponse.json(ResponseModel, { status: 201 })
+}
--- app/api/user/service/rule_service.js
+++ app/api/user/service/rule_service.js
@@ -1,15 +1,14 @@
-import { GetById } from "../../../../lib/supabase/crud";
-import { RuleResult } from "../../../../lib/model/Rule";
+import { Create, GetById } from "../../../../lib/supabase/crud";
+import { RuleResult, RuleBased } from "../../../../lib/model/Rule";
 import { CreateClientSecret } from "../../../../lib/supabase/client";
 
-
 const db = CreateClientSecret();
-export async function QueryRule(ruleID, index){
-    try{ 
-        const response = await GetById(db,"TestRuleBasedFolkFolk", ruleID);
-        if(response.error){
+export async function QueryRule(ruleID, index) {
+    try {
+        const response = await GetById(db, "TestRuleBasedFolkFolk", ruleID);
+        if (response.error) {
             console.error("Error fetching rule:", response.error);
-            return {data: null, error: response.error.message};
+            return { data: null, error: response.error.message };
         }
         RuleResult.id = response.data[0].id;
         RuleResult.location = response.data[0].location;
@@ -17,10 +16,30 @@ export async function QueryRule(ruleID, index){
         RuleResult.predicted_genotype = response.data[0].predicted_genotype[index];
         RuleResult.predicted_phenotype = response.data[0].predicted_phenotype[index];
         RuleResult.recommendation = response.data[0].recommend[index];
-        return {data: RuleResult, error: null};
-
-    }catch(error){
+        return { data: RuleResult, error: null };
+    } catch (error) {
         console.error("Error fetching rule:", error);
-        return {data: null, error: error.message};
+        return { data: null, error: error.message };
+    }
+}
+
+export async function InsertRule(InsertRuleModel) {
+    try {
+        const response = await Create(db, "TestRuleBasedFolkFolk", InsertRuleModel);
+        if (response.error) {
+            console.error("Error inserting rule:", response.error);
+            return { data: null, error: response.error.message }; //for User
+        }
+        RuleBased.id = response.data[0].id;
+        RuleBased.location = response.data[0].location;
+        RuleBased.result_location = response.data[0].result_location;
+        RuleBased.predicted_genotype = response.data[0].predicted_genotype;
+        RuleBased.predicted_phenotype = response.data[0].predicted_phenotype;
+        RuleBased.recommendation = response.data[0].recommend;
+        RuleBased.created_at = response.data[0].created_at;
+        return { data: RuleBased, error: null }; //for User
+    } catch (error) {
+        console.error("Error inserting rule:", error);
+        return { data: null, error: error.message }; //for User
     }
 }
