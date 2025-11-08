84cfa8ebfefc35f82e54a3a2773bcc2c2de3291c
Ing3567 <phongphol.suksomphong@g.swu.ac.th>
2025-11-08 23:32:36 +0700

    add doctor

--- lib/fetch/type.ts
+++ lib/fetch/type.ts
@@ -1,6 +1,6 @@
 import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
 
-export type Storage ={
+export type Storage = {
     id: string;
     patient_id: string;
     fridge_id: string;
@@ -31,45 +31,45 @@ export type RuleBased = {
 }
 
 export type Report = {
-  id: string;
-  specimens_id: string;
-  specimen_name: string;
-  specimens_expire_date: string;
-  doctor_id: string;
-  doctor_fullname: string;
-  patient_id: string;
-  Eng_name: string;
-  Thai_name: string;
-  DOB: string;
-  age: number;
-  gender: string;
-  phone: string;
-  address: string;
-  pharm_verify: boolean;
-  medtech_verify: boolean;
-  note_id: string;
-  note_method: string;
-  index_rule: number;
-  rule_location: string;
-  rule_result_location: string;
-  rule_phenotype: string;
-  rule_predicted_genotype: string;
-  rule_predicted_phenotype: string;
-  rule_recommendation: string;
-  rule_id: string;
-  rule_name: string;
-  pharmacist_id: string;
-  fullname_pharmacist: string;
-  medical_technician_id: string;
-  fullname_medtech: string;
-  more_information: string;
-  status: string;
-  request_date: string;
-  report_date: string;
-  priority: string;
-  ward_id: string;
-  created_at: string;
-  updated_at: string;
+    id: string;
+    specimens_id: string;
+    specimen_name: string;
+    specimens_expire_date: string;
+    doctor_id: string;
+    doctor_fullname: string;
+    patient_id: string;
+    Eng_name: string;
+    Thai_name: string;
+    DOB: string;
+    age: number;
+    gender: string;
+    phone: string;
+    address: string;
+    pharm_verify: boolean;
+    medtech_verify: boolean;
+    note_id: string;
+    note_method: string;
+    index_rule: number;
+    rule_location: string;
+    rule_result_location: string;
+    rule_phenotype: string;
+    rule_predicted_genotype: string;
+    rule_predicted_phenotype: string;
+    rule_recommendation: string;
+    rule_id: string;
+    rule_name: string;
+    pharmacist_id: string;
+    fullname_pharmacist: string;
+    medical_technician_id: string;
+    fullname_medtech: string;
+    more_information: string;
+    status: string;
+    request_date: string;
+    report_date: string;
+    priority: string;
+    ward_id: string;
+    created_at: string;
+    updated_at: string;
 }
 
 export type Note = {
@@ -79,25 +79,25 @@ export type Note = {
 }
 
 export type Patient = {
-  id: string;
-  Eng_name: string;
-  Thai_name: string;
-  phone: string 
-  age: number 
-  gender: "Male" | "Female" | string; 
-  Ethnicity: string 
-  dob: string  
-  email: string 
-  address: string 
-  created_at: string;
-  updated_at: string;
+    id: string;
+    Eng_name: string;
+    Thai_name: string;
+    phone: string
+    age: number
+    gender: "Male" | "Female" | string;
+    Ethnicity: string
+    dob: string
+    email: string
+    address: string
+    created_at: string;
+    updated_at: string;
 }
 
 export type HLA_B = {
     id: string;
     hla_gene: string[];
     drug: string[];
-    typeof_scar: string[]; 
+    typeof_scar: string[];
     ethnic_group: string[];
     odds_score: string[];
     ref: string[];
@@ -109,4 +109,12 @@ export type Qualityper = {
     fail: number,
     warning: number,
     total: number
+}
+
+export type Doctor = {
+    id: string;               
+    email: string;            
+    position: string;         
+    fullname: string;         
+    created_at: string; 
 }
